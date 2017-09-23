df4979ac9ddb8d5362edb1ffd77843f25dc6baa8
mondeer <keringlaban@yahoo.com>
2017-09-23 22:53:22 +0300

    Integrated Tangerine font to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,12 +9,12 @@
     </div>
     <div class="row">
         <div class="col-md-4 imondtext">
-            <p></p>
-            <blockquote>We, at Royal Tierra, are result driven and relationship based. 
+            <p>We, at Royal Tierra, are result driven and relationship based. 
             This means that you, our client,whether a seller, buyer, mandate or 
             financier are a valued member of our global network.Together, we 
             will follow simple, clear processes that benefit and protect us, to 
-            engage in efficient,reliable communications and deliver targeted results.</blockquote>
+            engage in efficient,reliable communications and deliver targeted results.</p>
+            <blockquote></blockquote>
         </div>
     </div>
 </div>
