1fc6b076c2ed01d42fc9a5da5e12264137e9303e
mondeer <keringlaban@yahoo.com>
2017-09-23 21:43:29 +0300

    added route about

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,298 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>ICAISP</title>
+  <meta name="description" content=" International China Africa Investment Service Portal.">
+  <meta name="keywords" content=" International China Africa Investment Service Portal.">
+  <meta name="author" content="ICAISP">
+  <link rel="icon" href="images/imond.png">
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet"> -->
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Corousel  -->
+	<link rel="stylesheet" href="css/carousel.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+
+	</head>
+	<body>
+
+	<div class="gtco-loader"></div>
+
+	<div id="page">
+		<div class="">
+	      <div class="bg-color">
+	        <div class="row">
+
+	        </div>
+	        <header id="main-header">
+	        <nav class="gtco-nav navbar navbar-default navbar-fixed-top">
+	          <div class="container">
+
+	            <div class="navbar-header">
+	              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#imondMenu">
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	              </button>
+	           	</div>
+	            <div class="collapse navbar-collapse" id="imondMenu">
+								<div class="row">
+									<div class="col-sm-4 col-xs-12">
+										<div id="gtco-logo"><img class="" src="{{ url ('images/imond.png')}}" height="80px"></div>
+									</div>
+									<div class="col-xs-8 text-right menu-1"><br><br>
+										<ul>
+											<li class="active"><a href="/">Home</a></li>
+											<li><a href="/about">About</a></li>
+											<li><a href="/Portfolio">Portfolio</a></li>
+											<li><a href="/contact">Contact</a></li>
+										</ul>
+									</div>
+								</div>
+	            </div>
+	          </div>
+	        </nav>
+	        </header>
+	        <!-- <div class="wrapper">
+	        <div class="container">
+	          <div class="row"> -->
+						<div id="bootstrap-touch-slider" class="carousel bs-slider fade  control-round indicators-line" data-ride="carousel" data-pause="hover" data-interval="5000" >
+
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+							<li data-target="#bootstrap-touch-slider" data-slide-to="0" class="active"></li>
+							<li data-target="#bootstrap-touch-slider" data-slide-to="1"></li>
+							<li data-target="#bootstrap-touch-slider" data-slide-to="2"></li>
+					</ol>
+
+					<!-- Wrapper For Slides -->
+					<div class="carousel-inner" role="listbox">
+
+							<!-- Third Slide -->
+							<div class="item active">
+
+									<!-- Slide Background -->
+									<img src="{{ url ('/images/ven3.jpg')}}" alt=""  class="slide-image"/>
+									<div class="bs-slider-overlay"></div>
+
+									<div class="container">
+											<div class="row">
+													<!-- Slide Text Layer -->
+													<div class="slide-text slide_style_left">
+															<h1 data-animation="animated zoomInRight">International China-Africa <br>Investment Service Portal</h1>
+															<p data-animation="animated fadeInLeft"><em>A Limited Company Incorporated in the republic Kenya under the companies act(cap 486)</em></p>
+															<a href="#" class="btn btn-default" data-animation="animated fadeInLeft">Learn More</a>
+													</div>
+											</div>
+									</div>
+							</div>
+							<!-- End of Slide -->
+
+							<!-- Second Slide -->
+							<div class="item">
+
+									<!-- Slide Background -->
+									<img src="{{ url ('/images/venture.jpg')}}" alt=""  class="slide-image"/>
+									<div class="bs-slider-overlay"></div>
+									<!-- Slide Text Layer -->
+									<div class="slide-text slide_style_left">
+											<h1 data-animation="animated zoomInRight">International China-Africa <br>Investment Service Portal</h1>
+											<p data-animation="animated fadeInLeft"><em>A Limited Company Incorporated in the republic Kenya under the companies act(cap 486)</em></p>
+											<a href="#" class="btn btn-default" data-animation="animated fadeInLeft">Learn More</a>
+									</div>
+							</div>
+
+							<div class="item">
+
+									<!-- Slide Background -->
+									<img src="{{ url ('/images/gem.jpg')}}" alt=""  class="slide-image"/>
+									<div class="bs-slider-overlay"></div>
+									<!-- Slide Text Layer -->
+									<div class="slide-text slide_style_left">
+											<h1 data-animation="animated zoomInRight">International China-Africa <br>Investment Service Portal</h1>
+											<p data-animation="animated fadeInLeft"><em>A Limited Company Incorporated in the republic Kenya under the companies act(cap 486)</em></p>
+											<a href="#" class="btn btn-default" data-animation="animated fadeInLeft">Learn More</a>
+									</div>
+							</div>
+							<!-- End of Slide -->
+
+
+
+
+					</div><!-- End of Wrapper For Slides -->
+
+					<!-- Left Control -->
+					<a class="left carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="prev">
+							<span class="fa fa-angle-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+					</a>
+
+					<!-- Right Control -->
+					<a class="right carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="next">
+							<span class="fa fa-angle-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+					</a>
+
+			</div> <!-- End  bootstrap-touch-slider Slider -->
+			<!-- <div class="col-md-12 wow fadeIn delay-05s">
+
+							  <div class="banner-text">
+	                <h2> <span> International China-Africa <br>Investment Service Portal.</span> </h2>
+									<h2><em>A Step Closer</em></h2>
+									<h4><em>A Limited Company Incorporated in the republic Kenya under the companies act(cap 486)</em></h4>
+	               </div>
+	            </div> -->
+	          </div>
+	          </div>
+	        <!-- </div>
+	        </div>
+	      </div> -->
+        @yield('content')
+	<div id="gtco-subscribe">
+		<div class="gtco-container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+					<h2>Subscribe</h2>
+					<p>Be the first to know about the new templates.</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-12">
+					<form class="form-inline">
+						<div class="col-md-4 col-sm-4">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Your Email">
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="form-group">
+								<label for="name" class="sr-only">Name</label>
+								<input type="text" class="form-control" id="name" placeholder="Your Name">
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="gtco-footer" role="contentinfo">
+		<div class="gtco-container">
+			<div class="row row-p	b-md">
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3>About Us</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eos molestias quod sint ipsum possimus temporibus officia iste perspiciatis consectetur in fugiat repudiandae cum. Totam cupiditate nostrum ut neque ab?</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-md-push-1">
+					<div class="gtco-widget">
+						<h3>Links</h3>
+						<ul class="gtco-footer-links">
+							<li><a href="#">Knowledge Base</a></li>
+							<li><a href="#">Career</a></li>
+							<li><a href="#">Press</a></li>
+							<li><a href="#">Terms of services</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3>Get In Touch</h3>
+						<ul class="gtco-quick-contact">
+							<li><a href="#"><i class="icon-phone"></i> +254 718 369 742</a></li>
+							<li><a href="#"><i class="icon-mail2"></i> info@icaisp.com</a></li>
+
+						</ul>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12">
+					<p class="pull-left">
+						<small class="block">&copy; 2017 ICAISP. All Rights Reserved.</small>
+						<small class="block">Designed by <a href="http://www.imond.co.ke" target="_blank">iMond Solutions</a></small>
+					</p>
+					<p class="pull-right">
+						<ul class="gtco-social-icons pull-right">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
